4df7a9f095557cdb0954e0cbf71928ccea9c2bb0
Dash433 <John@thomas-household.com>
2018-05-14 01:42:51 -0500

    code
    code

---
+++ Coin flip.java
@@ -0,0 +1,9 @@
+
+public class Coin flip
+    
+   public static void main([] args)
+   {
+      if (Math.random()) > 0.5) system.out.print( "true");
+      else System.out.print( "false");
+   }
+}    
